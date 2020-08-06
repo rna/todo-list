c4a94e78f4e9664b34cb43dad37998191772cd01
Ramesh <rameshnaidu.allu@gmail.com>
2020-08-06 23:51:51 +0530

    merge conflict

--- src/index.js
+++ src/index.js
@@ -7,38 +7,14 @@ import { projectList, listNewProject } from './projectList';
 let projects = [];
 const defaultProject = new Project("Inbox");
 projects.push(defaultProject);
+
 const rendr = document.getElementById('container');
-<<<<<<< HEAD
-=======
-const radioForm = document.createElement('form');
-radioForm.id = 'radioForm';
->>>>>>> c463940ccbf72d86d6d7b60eff0023566d0f9de1
 
 rendr.appendChild(projectForm());
 rendr.appendChild(todoForm());
 
 const radioForm = document.getElementById('doForm');
 rendr.appendChild(projectList(radioForm, projects));
-
-document.getElementById('Inbox').checked = true;
-
-
-function getRadioVal(form, name) {
-  var val;
-
-  var radios = form.elements[name];
-  console.log(radios)
-  var len = radios.length;
-  for (var i = 0; i < len; i++) {
-    if (radios[i].checked == true) {
-      val = radios[i].value;
-
-      break;
-    }
-  }
-
-  return val;
-}
 
 //create a new project
 const projectF = document.getElementById('projectform');
@@ -58,13 +34,6 @@ createtask.addEventListener('submit', (e) => {
   const description = document.getElementById('description').value;
   const dueDate = document.getElementById('date').value;
   const priority = document.getElementById('priority').value;
-<<<<<<< HEAD
-=======
-  //get a project
-  x = document.getElementById('radioForm');
-  const project = getRadioVal(x, 'project');
-  
->>>>>>> c463940ccbf72d86d6d7b60eff0023566d0f9de1
 
   //get a project
   const projectName = document.querySelector('input[name="project"]:checked').value;
@@ -74,6 +43,3 @@ createtask.addEventListener('submit', (e) => {
   console.log(projects);
   e.preventDefault();
 })
-let r = document.getElementById('radioForm');
-var valy = getRadioVal(r, 'project' );
-console.log(valy);
